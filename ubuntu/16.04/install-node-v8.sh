56254ef802c650d84b5c87be8d20e027b868f2ae
MJC <mattjcowan@users.noreply.github.com>
2017-11-20 23:15:06 -0600

    Update install-node-v8.sh

--- ubuntu/16.04/install-node-v8.sh
+++ ubuntu/16.04/install-node-v8.sh
@@ -3,6 +3,10 @@
 # Install nvm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Install node
 nvm install v8.9.1
 nvm alias default v8.9.1
