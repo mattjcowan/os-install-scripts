3d69a3face6975fecfe217dfb7bc762fc229039f
MJC <mattjcowan@users.noreply.github.com>
2017-11-26 10:08:26 -0600

    Update install-mysql.sh

--- ubuntu/16.04/install-mysql.sh
+++ ubuntu/16.04/install-mysql.sh
@@ -3,15 +3,63 @@
 # a great set of instructions also available here:
 # https://www.rosehosting.com/blog/install-cockpit-on-ubuntu-16-04/
 
-MYSQL_PASSWORD=Pa$$w0rd
+MYSQL_PASSWORD=root
 
 sudo apt-get update && sudo apt-get -y upgrade
 
-# Install mysql (although not necessary for Cockpit CMS which uses sqlite instead)
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_PASSWORD'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD'
-sudo apt-get update
+# Set the Server Timezone to CST
+echo "America/Chicago" > /etc/timezone
+sudo dpkg-reconfigure -f noninteractive tzdata
+
+# Install essential packages
+sudo apt-get -y install zsh htop
+
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
 sudo apt-get -y install mysql-server
 
-# further secure the mysql install with
-# mysql_secure_installation
+# Install mysql (although not necessary for Cockpit CMS which uses sqlite instead)
+# sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password $MYSQL_PASSWORD'
+# sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD'
+# sudo apt-get -y install mysql-server
+
+sudo apt-get -y install ufw
+sudo ufw allow ssh
+sudo ufw allow 3306
+sudo ufw --force enable
+
+# further secure the mysql install
+# this is for MySQL 5.7
+sudo apt-get -y install aptitude
+sudo aptitude -y install expect
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter password for user root:\"
+send \"$MYSQL_PASSWORD\r\"
+expect \"Would you like to setup VALIDATE PASSWORD plugin?\"
+send \"n\r\" 
+expect \"Change the password for root ?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+sudo aptitude -y purge expect
+
+sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/my.cnf
+# TODO
+# mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
+
+sudo service mysql restart
