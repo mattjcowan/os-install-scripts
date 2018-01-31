fa7808866efa9ab1e8f9f4efa25bbcb34522d514
MJC <mattjcowan@users.noreply.github.com>
2018-01-31 15:09:13 -0600

    Update install-node-v8.sh

--- ubuntu/16.04/install-node-v8.sh
+++ ubuntu/16.04/install-node-v8.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+# -----------------------------
+# INSTRUCTIONS
+# -----------------------------
+# DOWNLOAD_URL_BASE=https://raw.githubusercontent.com/mattjcowan/os-install-scripts/master/ubuntu/16.04
+# curl $DOWNLOAD_URL_BASE/install-node-v8.sh | bash
+# -----------------------------
+
 # Install nvm
-curl https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Install node
-nvm install v8.9.1
-nvm alias default v8.9.1
+nvm install v8.9.4
+nvm alias default v8.9.4
 nvm use default
 npm i -g rimraf dotenv pm2 forever nodemon
