ef498c700663ba80f697a1920fa0d95fcb8b16d8
MJC <mattjcowan@users.noreply.github.com>
2019-11-13 23:32:58 -0600

    apt-get for consistency

--- ubuntu/install-nodejs.sh
+++ ubuntu/install-nodejs.sh
@@ -22,5 +22,5 @@ sudo apt-get install -y build-essential
 # install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update -y
-sudo apt install yarn -y
+sudo apt-get update -y
+sudo apt-get install yarn -y
