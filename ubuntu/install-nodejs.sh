891109f5f546a0d1cf3878d314056b04ce3e238d
MJC <mattjcowan@users.noreply.github.com>
2019-11-13 23:28:37 -0600

    Added yarn

--- ubuntu/install-nodejs.sh
+++ ubuntu/install-nodejs.sh
@@ -12,6 +12,15 @@ if [ ! -v NODE_VERSION ]; then NODE_VERSION=12; fi
 
 sudo apt-get install curl -y
 
+# install nodejs
 curl -sL https://deb.nodesource.com/setup_${NODE_VERSION}.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# install build tools
 sudo apt-get install -y build-essential
+
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -y
+sudo apt install yarn -y
