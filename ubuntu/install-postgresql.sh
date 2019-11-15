baeae2c605811fecdf393d6fa4dc35e6e0fcded2
MJC <mattjcowan@users.noreply.github.com>
2019-11-15 13:32:30 -0600

    Update install-postgresql.sh

--- ubuntu/install-postgresql.sh
+++ ubuntu/install-postgresql.sh
@@ -5,7 +5,7 @@
 # -----------------------------
 # The following variables are the defaults, modify as needed
 # export PG_PASSWORD='YuiXQlk_d@WushaIDO@as^asSIOus'  # required
-# export PG_ALLOW_REMOTE_CONNECTIONS=yes # options: no, yes (default)
+# export PG_REMOTE_CONNECTIONS=yes # options: no, yes (default)
 # export PG_VERSION=11  # possible options: 9.6, 10, 11, 12
 # wget -O - https://raw.githubusercontent.com/mattjcowan/os-install-scripts/master/ubuntu/install-postgresql.sh | bash
 # -----------------------------
@@ -16,7 +16,7 @@ OS_CODENAME=$(grep -oP '(?<=^UBUNTU_CODENAME=).+' /etc/os-release | tr -d '"')
 
 if [ ! -v PG_VERSION ]; then PG_VERSION=11; fi
 if [ ! -v PG_PASSWORD ]; then PG_PASSWORD='YuiXQlk_d@WushaIDO@as^asSIOus'; fi
-if [ ! -v PG_ALLOW_REMOTE_CONNECTIONS ]; then PG_ALLOW_REMOTE_CONNECTIONS=yes; fi
+if [ ! -v PG_REMOTE_CONNECTIONS ]; then PG_REMOTE_CONNECTIONS=yes; fi
 
 sudo apt-get update -y
 sudo apt-get install curl ufw apt-transport-https -y
@@ -39,7 +39,7 @@ sudo service postgresql restart
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '$PG_PASSWORD';"
 
 # enable remote connections
-if [ "$PG_ALLOW_REMOTE_CONNECTIONS" == "yes" ]; then
+if [ "$PG_REMOTE_CONNECTIONS" == "yes" ]; then
   sudo sed -i "s/^#listen_addresses.*/listen_addresses = '*'/g" /etc/postgresql/${PG_VERSION}/main/postgresql.conf
   if [[ -z $(sudo grep "0.0.0.0/0" /etc/postgresql/${PG_VERSION}/main/pg_hba.conf) ]]; then 
     echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/${PG_VERSION}/main/pg_hba.conf
@@ -52,7 +52,8 @@ fi
 # restart service
 sudo service postgresql restart
 
-if [ "$PG_ALLOW_REMOTE_CONNECTIONS" == "yes" ]; then
+# open the firewall to allow external connections
+if [ "$PG_REMOTE_CONNECTIONS" == "yes" ]; then
   if [[ $(sudo ufw status) != *"OpenSSH"* ]]; then sudo ufw allow OpenSSH; fi
   sudo ufw allow 5432
   sudo ufw allow 5432/tcp
