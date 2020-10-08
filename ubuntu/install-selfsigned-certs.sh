1605f48741ee6e0b003bc6941f2a8898a98afe62
MJC <mattjcowan@users.noreply.github.com>
2020-10-08 16:43:16 -0500

    Update install-selfsigned-certs.sh

--- ubuntu/install-selfsigned-certs.sh
+++ ubuntu/install-selfsigned-certs.sh
@@ -1,42 +1,62 @@
 !/usr/bin/env bash
 
-sudo apt-get install openssl
+# -----------------------------
+# INSTRUCTIONS
+# -----------------------------
+# curl -sS https://raw.githubusercontent.com/mattjcowan/os-install-scripts/master/ubuntu/install-selfsigned-certs.sh | bash
+# wget -O - https://raw.githubusercontent.com/mattjcowan/os-install-scripts/master/ubuntu/install-dotnet.sh | bash
+# -----------------------------
 
-if [[ ! -f /etc/ssl/certs/dhparam.pem ]]; then
-sudo openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 4096 > /dev/null 2>&1
-fi
+sudo apt-get install nginx openssl -y
 
-if [[ ! -f /etc/ssl/private/nginx-selfsigned.key || ! -f /etc/ssl/certs/nginx-selfsigned.crt ]]; then
-# Generate a passphrase
-cd ~/
-sudo openssl rand -base64 48 > passphrase.txt
-# Generate a Private Key
-sudo openssl genrsa -aes128 -passout file:passphrase.txt -out server.key 2048
-# Generate a CSR (Certificate Signing Request)
-sudo openssl req -new -passin file:passphrase.txt -key server.key -out server.csr \
-    -subj "/C=US/ST=Illinois/L=Chicago/O=Startup/CN=ServerApp"
-# Remove Passphrase from Key
-sudo cp server.key server.key.org
-sudo openssl rsa -in server.key.org -passin file:passphrase.txt -out server.key
-# Generating a Self-Signed Certificate for 100 years
-sudo openssl x509 -req -days 36500 -in server.csr -signkey server.key -out server.crt
-
-# Rename and move the certs to a good destination
+# Create target directories to a good destination
 sudo mkdir -p /etc/nginx/ssl
 sudo mkdir -p /etc/nginx/ssl/private
 sudo chmod 755 /etc/nginx/ssl
 sudo chmod 710 /etc/nginx/ssl/private
 
+if [[ ! -f /etc/nginx/ssl/certs/dhparam.pem ]]; then
+sudo openssl dhparam -dsaparam -out /etc/nginx/ssl/certs/dhparam.pem 4096 > /dev/null 2>&1
+fi
+
+if [[ ! -f /etc/nginx/ssl/private/nginx-selfsigned.key || ! -f /etc/nginx/ssl/certs/nginx-selfsigned.crt ]]; then
+# Generate a passphrase
+cd ~/
+sudo openssl rand -base64 48 > passphrase.txt
+
+# Generate a Private Key
+sudo openssl genrsa -aes128 -passout file:passphrase.txt -out server.key 2048
+
+# Generate a CSR (Certificate Signing Request)
+sudo openssl req -new -passin file:passphrase.txt -key server.key -out server.csr -subj "/C=US/ST=Illinois/L=Chicago/O=Startup/CN=ServerApps"
+
+# Remove Passphrase from Key
+sudo cp server.key server.key.org
+sudo openssl rsa -in server.key.org -passin file:passphrase.txt -out server.key
+
+# Generating a Self-Signed Certificate for 100 years
+sudo openssl x509 -req -days 36500 -in server.csr -signkey server.key -out server.crt
+
+# Move the generated certs
 sudo mv server.crt /etc/nginx/ssl/certs/nginx-selfsigned.crt
 sudo mv server.key /etc/nginx/ssl/private/nginx-selfsigned.key
 
+# Protect the cert and private key
 sudo chown -R root:root /etc/nginx/ssl/
 sudo chown -R root:ssl-cert /etc/nginx/ssl/private/
 sudo chmod 644 /etc/nginx/ssl/*.crt
 sudo chmod 640 /etc/nginx/ssl/private/*.key
 fi
 
-# Create snippet for easy integration to new sites
+# Create SSL selfsigned cert snippet for easy integration to new sites
+if [[ ! -f /etc/nginx/snippets/ssl-selfsigned.conf ]]; then
+sudo bash -c 'cat >/etc/nginx/snippets/ssl-selfsigned.conf' <<EOL
+ssl_certificate /etc/nginx/ssl/certs/nginx-selfsigned.crt;
+ssl_certificate_key /etc/nginx/ssl/private/nginx-selfsigned.key;
+EOL
+fi
+
+# Create SSL params snippet for easy integration to new sites
 if [[ ! -f /etc/nginx/snippets/ssl-params.conf ]]; then
 sudo bash -c 'cat >/etc/nginx/snippets/ssl-params.conf' <<EOL
 gzip off;
@@ -58,12 +78,5 @@ resolver_timeout 5s;
 add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";
 add_header X-Frame-Options SAMEORIGIN;
 add_header X-Content-Type-Options nosniff;
-EOL
-fi
-
-if [[ ! -f /etc/nginx/snippets/ssl-selfsigned.conf ]]; then
-sudo bash -c 'cat >/etc/nginx/snippets/ssl-selfsigned.conf' <<EOL
-ssl_certificate /etc/nginx/ssl/certs/nginx-selfsigned.crt;
-ssl_certificate_key /etc/nginx/ssl/private/nginx-selfsigned.key;
 EOL
 fi
